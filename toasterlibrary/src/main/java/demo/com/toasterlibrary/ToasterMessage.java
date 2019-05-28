64e3f6ce116bf6831698277ab41a3cee7094654a
Vasim <vasim786diwan@gmail.com>
2019-05-28 20:34:43 +0530

    Initial Commit

---
+++ toasterlibrary/src/main/java/demo/com/toasterlibrary/ToasterMessage.java
@@ -0,0 +1,13 @@
+package demo.com.toasterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+    public static void s(Context context,String message){
+
+        Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+
+    }
+}
